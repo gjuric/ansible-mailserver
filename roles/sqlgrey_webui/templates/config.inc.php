a247269ee1932e880e8cf977bf6f00dc8d80b271
gjuric <gjuric@gmail.com>
2015-01-09 18:30:11 +0100

    Update sgwi (sqlgrey web interface) to 1.1.7

--- roles/sqlgrey_webui/templates/config.inc.php
+++ roles/sqlgrey_webui/templates/config.inc.php
@@ -2,23 +2,23 @@
 
 /***************************************************
 SQLgrey Web Interface
-Filename:	config.inc.php
-Purpose: 	Configuration database and options
-Version: 	1.1.6
+Filename:   config.inc.php
+Purpose:    Configuration of database and options
+Version:    1.1.7
 ****************************************************/
 
 /* Database settings */
-$db_db		= "{{ greylisting_db_name }}";
-$db_hostname	= "{{ greylisting_db_host }}";
-$db_user	= "{{ greylisting_db_username }}";
-$db_pass	= "{{ greylisting_db_password}}";
-$db_type	= "mysql";	// mysql or pg (pg=postgress)
+$db_hostname   = "{{ greylisting_db_host }}";
+$db_db         = "{{ greylisting_db_name }}";
+$db_user       = "{{ greylisting_db_username }}";
+$db_pass       = "{{ greylisting_db_password}}";
+$db_type       = "mysql";  // "mysql" or "pg" (postgresql)
 
 /* Set close_btn to 'yes' to enable the close button in index.php (main menu)
    the button action = ../ which could be a security issue
-   default = no
+   default = "no"
 */
-$close_btn	= "no";
+$close_btn  = "no";
 
 /* Set no_millisecs to 'no' if your server's dbase shows milliseconds
    and you do want these to be displayed - this will take two lines per entry.
@@ -26,9 +26,9 @@ $close_btn	= "no";
    ('no' used to be the default and leaves the date format untouched).
    When set to 'yes' timestamps will be formatted as 'yyyy-mm-dd hh:mm:ss'
    which doubles the amount of visible entries.
-   default = yes
+   default = "yes"
 */
-$no_millisecs	= "yes";
+$no_millisecs  = "yes";
 
 /* Depending on your PHP version you may have to set default timezone to avoid warnings.
    Remove the comment (//) and change the default to your region.
